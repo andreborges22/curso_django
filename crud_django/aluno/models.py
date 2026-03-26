f73d76ec83b550f7350ead798c95d939f630aa76
andreborges22 <andreborges22@gmail.com>
2026-03-26 18:06:20 -0300

    comentando o model aluno e sexo

--- crud_django/aluno/models.py
+++ crud_django/aluno/models.py
@@ -6,11 +6,16 @@ from django.db import models
 
 
 class Aluno(models.Model):
-    # definicao do campo para armazenar o nome do Aluno
+    #definicao dos campos para armazenar os dados do Aluno
+    #campo nome do tipo texto
     nome = models.CharField(max_length=255)
+    #campo email, do tipo email, e configurado com unique (nao permite duplicacao)
     email = models.EmailField(max_length=100,default="sememail@gmail.com",unique="true")
+    #campo sexo vem do model sexo, com a protecao, ou seja, nao é possível deletar um registro da tabela sexo se ele estiver sendo  usado na tabela aluno
     sexo = models.ForeignKey("Sexo", default=1, on_delete=models.PROTECT)    
+    #campo telefone do tipo texto
     telefone = models.CharField(max_length=20,default="Não informado")
+    #campo telefone do tipo texto
     endereco = models.CharField(max_length=255,default="Não informado")
     
 
@@ -18,9 +23,10 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
+# model sexo para armazenar tipo de sexo
 class Sexo(models.Model):
     descricao = models.CharField(max_length=100)
 
-    # método para imprimir a descrição
+    # método para imprimir a descrição do sexo
     def __str__(self):
         return self.descricao
