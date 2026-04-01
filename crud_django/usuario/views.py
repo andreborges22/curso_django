431f7eb23e8f3bd0b7b2bb99fa50557ec364e45a
andreborges22 <andreborges22@gmail.com>
2026-04-01 19:33:07 -0300

    adicionando verificacao se usuario é admin

--- crud_django/usuario/views.py
+++ crud_django/usuario/views.py
@@ -7,28 +7,34 @@ from aluno.models import Sexo
 
 # Create your views here.
 def home(request):
-    sexos = Sexo.objects.all()
-    return render(request, 'usuario/home.html', {'sexos': sexos})
+    if not request.user.is_superuser:
+        return redirect('core:home')
+    else:
+        sexos = Sexo.objects.all()
+        return render(request, 'usuario/home.html', {'sexos': sexos})
 
 
 def cadastrar(request):
 
-    nome = request.POST.get('nome')
-    email = request.POST.get('email')
-    senha = request.POST.get('senha')
+    if request.method == "GET":
+        return redirect('usuario:home')
+    else:
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
 
-    try:
-        user = User.objects.filter(username=nome).first()
-        if (user):
-            messages.error(
-                request, f"já existe um usuário com esse nome ({nome})")
+        try:
+            user = User.objects.filter(username=nome).first()
+            if (user):
+                messages.error(
+                    request, f"já existe um usuário com esse nome ({nome})")
+                return redirect('usuario:home')
+            user = User.objects.create_user(
+                username=nome, email=email, password=senha)
+            user.save()
+            messages.success(
+                request, f"Usuário {nome} cadastrado com sucesso")
             return redirect('usuario:home')
-        user = User.objects.create_user(
-            username=nome, email=email, password=senha)
-        user.save()
-        messages.success(
-            request, f"Usuário {nome} cadastrado com sucesso")
-        return redirect('usuario:home')
-    except Exception as erro:
-        messages.error(request, f"({erro})")
-        return redirect('usuario:home')
+        except Exception as erro:
+            messages.error(request, f"({erro})")
+            return redirect('usuario:home')
