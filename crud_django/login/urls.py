302cd579e6ce467a1ad4f9e5ab66219abc34cbf4
andreborges22 <andreborges22@gmail.com>
2026-04-02 22:23:04 -0300

    ajustes do formulario de login

--- crud_django/login/urls.py
+++ crud_django/login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+# namespace do app
 app_name = 'login'
 
+# roteamento
 urlpatterns = [
     path('',views.home, name='home'),
     path('login/',views.login, name='login'),
--- crud_django/login/views.py
+++ crud_django/login/views.py
@@ -8,29 +8,42 @@ from django.contrib.auth import login as login_django
 
 # Create your views here.
 
-
+#home do app
 def home(request):
+    # renderiza a página de login
     return render(request, 'login/login.html')
 
-
+#login no sistema
 def login(request):
+    # resgata dados do formulário
     nome = request.POST.get('nome')
     senha = request.POST.get('senha')
-
+    # tratamento de exceção
     try:
+        # tentando autenticar um usuário
         user = authenticate(username=nome, password = senha)
-        if user:            
+        # se usuário não for nulo
+        if user:   
+            # faz login de fato         
             login_django(request,user)
-            return redirect('core:home')
+            # redirecionando para a home
+            return render(request,'sisifba/home.html') 
+        #se o usuaio for vazio (None) a autenticação falhou
         else:
-            #return HttpResponse(f"Usuário ou senha incorretos")
+            # adicionando mensagem de erro para ser exibida na tela
             messages.error(
                 request, f"Usuário ou senha incorretos")
+            # redirecionando para o login
             return redirect('login:home')
+    # capturando exceção    
     except Exception as erro:
+        # adicionando mensagem de erro para ser exibida na tela
         messages.error(request, f"({erro})")
+        # redirecionando para o login
         return redirect('login:home')
-
+# função de logout
 def sair(request):    
+    # faz o logout do sistema
     logout(request)
+    # redirecionando para o login
     return redirect('login:home')  # ou qualquer página que você quiser
